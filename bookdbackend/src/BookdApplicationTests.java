3a3493703ad84a3d6779c1ebdc93463908a9c6b0
Benita <thomasbenita18@gmail.com>
2026-05-14 22:40:12 +0530

    Add initial test class for Bookd application

---
+++ bookdbackend/src/BookdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookd.bookd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
